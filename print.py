1e18bc7a47dda15e9c030c2c238e3a184f0908f6
Dmitriy Hrytsak <hrytsakdmitriy@gmail.com>
2019-09-24 21:28:29 +0300

    Поміняв якусь штуку

--- print.py
+++ print.py
@@ -8,6 +8,6 @@ def discounted(price, discount, max_discount=50):
         price_with_discount = price
     else:
         price_with_discount = price - price * discount / 100
-    return(price_with_discount)
+    print(price_with_discount)
 
 print(discounted(100, 40))
